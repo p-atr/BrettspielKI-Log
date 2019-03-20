8ab9b813b95435b866556eb67ff21982e3ab66f1
patr_ <patrickematik@gmail.com>
2019-03-20 21:53:06 +0100

    log v.0.0.2
    optimisazion for mobile devices

--- submit.php
+++ submit.php
@@ -7,35 +7,38 @@
   <link rel="icon" type="image/gif" href="favicon.gif">
   <link rel="stylesheet" type="text/css" href="timeline.css">
   <link rel="stylesheet" type="text/css" href="header.css">
+  <link rel="stylesheet" media="only screen and (max-width: 999px)" href="mobile.css" />
+  <link rel="stylesheet" media="only screen and (min-width: 1000px)" href="desktop.css" />
 </head>
 
 <body>
   <div class="header" id="topHeader">
-    <img style="float: left; margin-right: 1em;" height="80em" src="favicon.gif">
+    <img class="icon" src="favicon.gif">
     <h1>Submit - Logbuch Brettspiel-KI</h1>
   </div>
-    <div class="content" style="width:50%; margin:0 auto;">
+    <div class="content submit">
   <form onsubmit="form_generate()" action="submit.php" method="post" id="form">
-    <h3>Text:</h3>
-    <textarea rows="10" cols="80" id="l_text"></textarea><br>
-    <h3>Passwort:</h3>
-    <input type="password" id="l_password">
+    <h2>Text:</h2>
+    <textarea id="l_text"></textarea><br>
+    <h2>Passwort:</h2>
+    <input class="password" type="password" id="l_password">
     <input type="hidden" name="text" id="text">
     <input type="hidden" name="hmac" id="hmac"><br>
     <input type="submit">
   </form>
 
   <?php
-
-  include ('secret.php');
+  ob_start();
+  require('secret.php');
+  $useless = ob_get_clean();
 
   if (isset($_POST['text']) and isset($_POST['hmac'])) {
-    sleep(5);
     $local_hmac = hash_hmac('sha512', $_POST['text'], $secret);
     //$text = base64_decode($_POST['text']);
     $text= str_replace('LSWISSA','ä',str_replace('USWISSA','Ä',str_replace('LSWISSO','ö',str_replace('USWISSO','Ö',
     str_replace('LSWISSU','ü',str_replace('USWISSU','Ü',base64_decode($_POST['text']))))))); //äöü-Fix
-    file_put_contents('.htdatabase_log.txt', date('d.m.Y H:i:s').' [IP:'.$_SERVER['REMOTE_ADDR'].'] '.$text."\n", FILE_APPEND);
+    file_put_contents('.ht_database_log.txt', date('d.m.Y H:i:s').' [IP:'.$_SERVER['REMOTE_ADDR'].'] '.trim(preg_replace('/\s+/', ' ', $text))."\n", FILE_APPEND);
+    usleep(rand(4500, 5500)*1000);
 
     if ($local_hmac == $_POST['hmac']) {
         echo('CORRECT QUERY -> CONTINUE');
